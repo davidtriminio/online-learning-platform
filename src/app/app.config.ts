0d2c3821b82a61961f3a49de3171c0c3c591de4a
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-17 22:38:00 -0600

    feat(app): register routes and authinterceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideRouter, withComponentInputBinding } from '@angular/router'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { routes } from './app.routes'
+import { authInterceptor } from './features/auth/infrastructure/auth-interceptor'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
 };
