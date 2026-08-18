3d63ccdad6b131cbc36eb8a2a92f56ee644b352c
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-18 09:29:56 -0600

    feat(courses): define courses.routes setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,23 @@
-import { Routes } from '@angular/router'
+import { Routes } from '@angular/router';
 import { authGuard } from './features/auth/infrastructure/auth.guard';
 
 export const routes: Routes = [
-  {path: 'login', loadChildren: () => import('./features/auth/auth.routes').then(m => m.authRoutes)},
+  {
+    path: 'login',
+    loadChildren: () => import('./features/auth/auth.routes').then((m) => m.authRoutes),
+  },
+  {
+    path: 'courses',
+    canActivate: [authGuard],
+    loadChildren: () => import('./features/courses/courses.routes').then((m) => m.coursesRoutes),
+  },
   // {
   //   path: 'courses',
   //   canActivate: [authGuard],
   // },
   {
-    path: '', redirectTo: 'login', pathMatch: 'full'
-  }
-]
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
