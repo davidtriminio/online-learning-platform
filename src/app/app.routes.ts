9192117c9649a608f645830475f3d3a80966d0db
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-17 22:36:50 -0600

    feat(auth): add login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { authGuard } from './features/auth/infrastructure/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'login', loadChildren: () => import('./features/auth/auth.routes').then(m => m.authRoutes)},
+  // {
+  //   path: 'courses',
+  //   canActivate: [authGuard],
+  // },
+  {
+    path: '', redirectTo: 'login', pathMatch: 'full'
+  }
+]
