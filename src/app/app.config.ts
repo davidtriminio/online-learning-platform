e3fabc6a3bdf2288824e8e59587fbf6e49c781c9
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-18 07:49:22 -0600

    feat(core): implement apiErrorInterceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,12 @@ import { provideRouter, withComponentInputBinding } from '@angular/router'
 import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { routes } from './app.routes'
 import { authInterceptor } from './features/auth/infrastructure/auth.interceptor'
+import { apiErrorInterceptor } from './core/http/api-error-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([apiErrorInterceptor, authInterceptor])),
   ],
 };
