77b1ba6efd14d07c5b1ed4ae0fdb031566916c92
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-18 21:48:45 -0600

    feat(courses): add course-card component

---
+++ src/app/features/courses/ui/components/course-card/course-card.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { Course } from '../../../domain/course.model';
+import { CardContainer } from '../../../../../shared/ui/card-container/card-container';
+import { LucideCirclePlay, LucideClock } from '@lucide/angular';
+
+@Component({
+  selector: 'app-course-card',
+  imports: [CardContainer, LucideClock, LucideCirclePlay],
+  templateUrl: './course-card.html',
+})
+export class CourseCard {
+  readonly course = input.required<Course>();
+}
