00c1b88cdbed7ab5939f4d15f25f2b3435fb4e7e
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-18 22:39:02 -0600

    feat(shared): implement error-state UI component

---
+++ src/app/shared/ui/error-state/error-state.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { LucideRotateCw, LucideTriangleAlert } from '@lucide/angular';
+
+@Component({
+  selector: 'app-error-state',
+  imports: [LucideRotateCw, LucideTriangleAlert],
+  templateUrl: './error-state.html',
+})
+export class ErrorState {}
