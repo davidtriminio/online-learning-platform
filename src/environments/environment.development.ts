ff8a4e98ff095b7b3f5656987ff0e398e17c40df
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-19 20:14:43 -0600

    chore(dev): add API proxy to bypass CORS

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,5 +4,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
   production: false,
-  apiUrl: 'https://projectapi.gerasim.in/api/OnlineLearning/',
+  apiUrl: '/api/OnlineLearning/',
 }
