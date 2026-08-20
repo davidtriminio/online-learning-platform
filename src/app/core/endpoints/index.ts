86e6c0626c6cabf6a837473d742bcb4d5f1c3edd
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-20 13:35:05 -0600

    feat(core): add video endpoints

--- src/app/core/endpoints/index.ts
+++ src/app/core/endpoints/index.ts
@@ -1,2 +1,4 @@
 export * from './auth.endpoints'
 export * from './courses.endpoints'
+export * from './videos.endpoints'
+export * from './course-videos.endpoints'
---
+++ src/app/core/endpoints/videos.endpoints.ts
@@ -0,0 +1,6 @@
+export const VIDEO_ENDPOINTS = {
+  getAll: 'GetAllVideos',
+  add: 'AddNewVideo',
+  update: 'UpdateVideo',
+  delete: 'DeleteVideo',
+} as const
