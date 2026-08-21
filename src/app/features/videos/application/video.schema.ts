bd18bb6c89f05ea5fc910e264d454b1bfd895e80
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-20 18:54:25 -0600

    feat(videos): add video form schema

---
+++ src/app/features/videos/application/video.schema.ts
@@ -0,0 +1,14 @@
+import { required, schema } from '@angular/forms/signals'
+
+export interface VideoForm {
+  url: string
+  title: string
+  description: string
+  thumbnail: string
+  duration: string
+}
+
+export const VideoSchema = schema<VideoForm>((path) => {
+  required(path.title, { message: 'Title is required.' })
+  required(path.url, { message: 'Video URl is required.' })
+})
