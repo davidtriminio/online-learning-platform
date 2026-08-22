69f2462bf473634af87cca71aeb15d8ec5fb8160
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-21 18:29:05 -0600

    feat(app): enable router view transitions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core'
-import { provideRouter, withComponentInputBinding } from '@angular/router'
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router'
 import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { routes } from './app.routes'
 import { authInterceptor } from './features/auth/infrastructure/auth.interceptor'
@@ -9,7 +9,7 @@ import { loadingInterceptor } from './core/loading/loading-interceptor'
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
     provideHttpClient(withInterceptors([apiErrorInterceptor, loadingInterceptor, authInterceptor])),
   ],
 }
