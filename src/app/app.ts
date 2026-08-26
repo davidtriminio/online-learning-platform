657e21df81231d0abecec9b91594ced638f93683
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-26 13:50:23 -0600

    chore(app): move theme toggle to navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, inject } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { ThemeService } from './infrastructure/theme/theme.service'
 import { LoadingOverlay } from './shared/ui/loading-overlay/loading-overlay'
-import { ThemeToggle } from './shared/ui/theme-toggle/theme-toggle'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoadingOverlay, ThemeToggle],
+  imports: [RouterOutlet, LoadingOverlay],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
