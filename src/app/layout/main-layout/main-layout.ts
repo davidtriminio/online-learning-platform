5cf320c555ec28098458c78e37991691deafa649
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-26 13:41:45 -0600

    feat(layout): add main layout shell

---
+++ src/app/layout/main-layout/main-layout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { Navbar } from '../navbar/navbar'
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [RouterOutlet, Navbar],
+  templateUrl: './main-layout.html',
+})
+export class MainLayout {}
