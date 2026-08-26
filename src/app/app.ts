ece38c5f3fbc6faee69e2aa448d1af223ea5de07
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-25 22:13:25 -0600

    feat(theme): fix media query, add system mode

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { ThemeService } from './infrastructure/theme/theme.service'
 import { LoadingOverlay } from './shared/ui/loading-overlay/loading-overlay'
+import { ThemeToggle } from './shared/ui/theme-toggle/theme-toggle'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoadingOverlay],
+  imports: [RouterOutlet, LoadingOverlay, ThemeToggle],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
