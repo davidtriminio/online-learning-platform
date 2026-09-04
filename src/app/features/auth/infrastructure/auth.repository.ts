f1d0d5a701d65a865f84ed5735acb41e32e9b343
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-09-04 09:03:32 -0600

    feat(auth): add register use case

--- src/app/features/auth/infrastructure/auth.repository.ts
+++ src/app/features/auth/infrastructure/auth.repository.ts
@@ -8,23 +8,36 @@ import { UserResponseDto } from './dto/user-response.dto'
 import { toAuthUser } from './mappers/auth-user.mapper'
 import { AUTH_ENDPOINTS } from '../../../core/endpoints'
 import { environment } from '../../../../environments/environment'
+import { RegisterForm } from '../application/register.schema'
+import { toRegisterRequestDto } from './mappers/register.mapper'
 
-export interface AuthSession{
+export interface AuthSession {
   user: AuthUser
   token: string
 }
 
-@Injectable({providedIn: 'root'})
-export class AuthRepository{
+@Injectable({ providedIn: 'root' })
+export class AuthRepository {
   private http = inject(HttpClient)
   private baseUrl = environment.apiUrl
 
-  login(credentials: LoginRequestDto): Observable<AuthSession>{
+  login(credentials: LoginRequestDto): Observable<AuthSession> {
     return this.http
-    .post<ApiResponse<UserResponseDto>>(`${this.baseUrl}${AUTH_ENDPOINTS.login}`, credentials)
-    .pipe(map(res => ({
-      user: toAuthUser(res.data),
-      token: res.data.refreshToken
-    })))
+      .post<ApiResponse<UserResponseDto>>(`${this.baseUrl}${AUTH_ENDPOINTS.login}`, credentials)
+      .pipe(
+        map((res) => ({
+          user: toAuthUser(res.data),
+          token: res.data.refreshToken,
+        })),
+      )
+  }
+
+  register(form: RegisterForm): Observable<AuthUser> {
+    return this.http
+      .post<ApiResponse<UserResponseDto>>(
+        `${this.baseUrl}${AUTH_ENDPOINTS.addNewUser}`,
+        toRegisterRequestDto(form),
+      )
+      .pipe(map((res) => toAuthUser(res.data)))
   }
 }
