21621e7b96d31418502287622fab8b188268495d
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-09-04 08:01:22 -0600

    feat(auth): add register dto and mapper

---
+++ src/app/features/auth/infrastructure/dto/register-request.dto.ts
@@ -0,0 +1,12 @@
+export interface RegisterRequestDto {
+  userId: number
+  userName: string
+  emailId: string
+  fullName: string
+  role: string
+  createdDate: string
+  password: string
+  projectName: string
+  refreshToken: string
+  refreshTokenExpiryTime: string
+}
